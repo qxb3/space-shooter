91a1c06b404bd4842695f9ba20e776289c596687
qxb3 <qxbthree@gmail.com>
2023-04-09 17:58:50 +0800

    fix randomNumber util

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import { exec } from 'child_process'
 
 export function randomNumber(min, max) {
-  return Math.random() * (max - min) + min
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 export function playMusic(fileName, repeat = 0) {
